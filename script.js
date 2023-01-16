faa72ab3c0f110e6b266eade22d3d354033606b4
Vishal Verma <100039120+vishal-dev0405@users.noreply.github.com>
2023-01-16 09:55:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // complete the given function
 
 function palindrome(str){
-
+const len = string.length;  
+    for (let i = 0; i < len / 2; i++) {  
+    
+        if (string[i] !== string[len - 1 - i]) {  
+            alert( 'It is not a palindrome');  
+        }  
+    }  
+    alert( 'It is a palindrome');  
 }
 module.exports = palindrome
