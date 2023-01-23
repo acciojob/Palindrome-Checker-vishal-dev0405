0c8e7f5ab4ad85fa1a0944e485d4ba3f4f3d73b5
Vishal Verma <100039120+vishal-dev0405@users.noreply.github.com>
2023-01-23 17:02:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 // complete the given function
+function isPalindrome(str) {
+    // convert the string to lowercase
+    str = str.toLowerCase();
+    // remove non-alphanumeric characters
+    str = str.replace(/[^a-z0-9]/gi, '');
+    // check if the string is a palindrome
+    return str === str.split('').reverse().join('');
+}
 
-function palindrome(str){
-const len = string.length;  
-    for (let i = 0; i < len / 2; i++) {  
-    
-        if (string[i] !== string[len - 1 - i]) {  
-            alert( 'It is not a palindrome');  
-        }  
-    }  
-    alert( 'It is a palindrome');  
+ 
 }
 module.exports = palindrome
